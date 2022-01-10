15e8002512030b1062d2a80deb16901f9f58b53c
maucoder <maugfalcon1@gmail.com>
2022-01-10 14:59:16 -0500

    adding js things

--- JS/Car.js
+++ JS/Car.js
@@ -1,6 +1,13 @@
-function Car(){
-    this.id;
-    this.name;
-    this.driver;
-    this.passenger;
+class Car {
+    constructor(license, driver){
+        this.license = license;
+        this.driver = driver;
+        this.id;
+        this.passenger;
+    }
+    printDataCar = () =>  {
+        console.log(this.driver)
+        console.log(this.driver.name)
+        console.log(this.driver.document)
+    }
 }
--- Java/Car.java
+++ Java/Car.java
@@ -1,8 +1,12 @@
 class Car {
     Integer id;
     String license;
-    String driver;
+    Account driver;
     Integer passenger;
+
+    public Car(String license, Account driver){
+        this.license = license;
+    }
 
     void printDataCar(){
         System.out.println("License: " + car.license " Driver: " + driver "passenger" + passeger);
