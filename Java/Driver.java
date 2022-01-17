051cd52baa998a41fe78c5b30e1e2f2efc57b007
maucoder <maugfalcon1@gmail.com>
2022-01-17 15:48:01 -0500

    adding files extention Java

---
+++ Java/Driver.java
@@ -0,0 +1,9 @@
+public class Driver extends Account{
+
+    public Driver(String name, String document, String email, String password) {
+        super(name, document);
+        this.email = email;
+        this.password = password;
+    }
+    
+}
