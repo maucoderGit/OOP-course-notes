ccfd104475fa8695cc4612e49afb50c7d3b6a7bc
maucoder <maugfalcon1@gmail.com>
2022-01-17 16:01:29 -0500

    fixing

--- Python/account.py
+++ Python/account.py
@@ -5,6 +5,8 @@ class Account:
     email: str
     password: str
 
-    def __init__(self, name, document):
+    def __init__(self, name, document, password, email):
         self.name = name
+        self.email = email
+        self.password = password
         self.document = document
